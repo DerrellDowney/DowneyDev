76f173787489437ecbdc347205bf379a6cb54f50
DerrellDowney <derrell.downey@gmail.com>
2023-05-22 13:46:20 -0400

    completed stack and queue python files

---
+++ data_structures_and_algorithms/1_stack_queue/queue.py
@@ -0,0 +1,15 @@
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if len(self.queue) > 0:
+            return self.queue.pop(0)
+        return None
+    
+    def size_of(self):
+        return len(self.queue)
---
+++ data_structures_and_algorithms/1_stack_queue/stack.py
@@ -0,0 +1,15 @@
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) > 0:
+            return self.stack.pop()
+        return None
+    
+    def size_of(self):
+        return len(self.stack)
